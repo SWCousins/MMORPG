21517699704f8dd549d05108e72bc23b897b4ef9
Spencer Cousins <scousins@2u.com>
2020-10-24 18:49:58 -0600

    added user templates

---
+++ userTemplates.js
@@ -0,0 +1,124 @@
+let warrior =
+    {
+        userName: '',
+            userId: 0,
+            charClass: 'warrior',
+            strength: 10,
+            HP: 100,
+            skills: 
+            {
+                bash: false,
+                rampage: false,
+                rush: false,
+                dodge: false,
+                defend: false,
+                heal: false,
+                parry: false,
+                
+            },
+            equipment:
+            [
+                
+            ],
+            inventory: 
+            [
+                
+            ],
+            acheivements: 
+            [
+                
+            ]
+    }
+
+let ranger =
+    {
+        userName: '',
+            userId: 0,
+            charClass: 'ranger',
+            strength: 5,
+            HP: 70,
+            pets: 
+            [
+                
+            ],
+            skills:
+            {
+                makeMap: false,
+                search: false,
+                detect: false,
+                makeSalve: false,
+                bullseye: false,
+                evade: false,
+                camoflage: false
+            },
+            items: 
+            [
+                
+            ],
+            acheivements: 
+            [
+                
+            ]
+    }
+
+let bard = 
+    {
+        userName: '',
+            userId: 0,
+            charClass: 'bard',
+            strength: 1,
+            HP: 50,
+            scrolls: 
+            [
+                
+            ],
+            skills:
+            {
+                battleSong: false,
+                barter: false,
+                enrapture: false,
+                illusion: false,
+                smokeBomb: false,
+                makeFlute: false,
+                playFool: false
+            },
+            items: 
+            [
+                
+            ],
+            acheivements: 
+            [
+                
+            ]
+    }
+
+let outcast =
+    {
+        userName: '',
+            userId: 0,
+            charClass: 'outcast',
+            strength: 7,
+            HP: 90,
+            henchmen: 
+            [
+                
+            ],
+            skills:
+            {
+                hide: false,
+                pickPocket: false,
+                backstab: false,
+                poison: false,
+                buildDen: false,
+                evade: false,
+                camoflage: false
+            },
+            items: 
+            [
+                
+            ],
+            acheivements: 
+            [
+                
+            ]
+    }
